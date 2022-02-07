82dc61f606bf68457f6642454fddd7a5da82acd1
msuleman67h <msuleman67h@gmail.com>
2022-02-07 15:27:43 -0500

    Code now trains with torch lstm
    Removed sklearn models and data preprocessing
    
    Everything (almost) is done by torch

--- src/main.py
+++ src/main.py
@@ -2,143 +2,104 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import accuracy_score, multilabel_confusion_matrix
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder
+import torch
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from torch.nn import CrossEntropyLoss
+from torch.nn.functional import one_hot
+import random
+
+from my_lstm import MyLSTM
 
 from music_dataset import MusicDataset
 
 
-def k_nearest_neighbors(X, y):
-    """This function does the nearest neighbor stuff"""
-
-    # Split the data into 80% train, 20% test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0)
-
-    # Set parameter grid to test over
-    param_grid = {
-        'n_neighbors': [1, 2, 3, 5, 7, 10],
-        'weights': ['uniform', 'distance'],
-        'p': [1, 2, 3]
-    }
-
-    # Create KNN Classifier and perform grid search cross validation
-    knn = KNeighborsClassifier()
-    knn_cv = GridSearchCV(knn, param_grid, verbose=10, n_jobs=10, cv=5)
-    knn_cv.fit(X_train, y_train)
-    y_pred = knn_cv.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    best_params = knn_cv.best_params_
-    per_class_confusions = multilabel_confusion_matrix(y_test, y_pred)
-
-    # Print out Gradient Boosted Tree report
-    print('-K Nearest Neighbors Report-')
-    print('\nKNN Overall Accuracy: ', accuracy)
-    print('\tBest Parameters:')
-    print('\t\tBest Number of Neighbors: ', best_params['n_neighbors'])
-    print('\t\tBest Weight Type: ', best_params['weights'])
-    print('\t\tBest Power Parameter (Minkowski): ', best_params['p'])
-    print('\tMulticlass metrics:')
-
-    # Print out individual class metrics
-    for i, confusion in enumerate(per_class_confusions):
-        tp = confusion[0][0]
-        fp = confusion[1][0]
-        fn = confusion[0][1]
-        tn = confusion[1][1]
-
-        accuracy = (tp + tn) / (tp + fp + fn + tn)
-        recall = tp / (tp + fn)
-        specificity = tn / (tn + fp)
-        precision = tp / (tp + fp)
-
-        print(f'\t\t{i} accuracy: ', accuracy)
-        print(f'\t\t{i} recall: ', recall)
-        print(f'\t\t{i} specificity: ', specificity)
-        print(f'\t\t{i} precision: ', precision)
-    print()
+def plot_mfccs(y, sr, title):
+    fig = plt.figure(figsize=(25, 10))
+    librosa.display.specshow(y,
+                             sr=sr,
+                             x_axis="time")
+    plt.colorbar(format="%+2.f")
+    plt.title(title, fontsize=24)
+    plt.ylabel("MFCCs Coefficients", fontsize=24)
+    plt.xlabel("Time (s)", fontsize=24)
+    fig.tight_layout()
+    plt.subplots_adjust(left=0.07, right=1.0)
+    plt.show()
 
 
-def gradient_boosted(X, y):
-    """
-    This function will find the best fit paramaters for a Gradient Boosted
-    Tree Classifier given preprocessed dataset. The model will be trained using
-    grid search w/ cross validation. Overall accuracy and individual class
-    metrics will be printed out at the end.
-
-    :param X: predictor data
-    :param y: target data corresponding to X
-    :return: None
-    """
-    # Set possible parameters for model tuning
-    parameters = {
-        'learning_rate': [0.01, 0.1, 0.2, 0.3],
-        'n_estimators': [1, 2, 4, 8, 16, 32, 64, 100, 200],
-        'max_depth': np.linspace(1, 10, 10, endpoint=True)
-    }
-    # Split 20% testing, 80% training
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    gbtc = GradientBoostingClassifier()
-    gbtc_cv = GridSearchCV(gbtc, parameters, verbose=10, n_jobs=10, cv=5)
-    gbtc_cv.fit(X_train, y_train)
-    y_pred = gbtc_cv.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-
-    confusion_matricies = multilabel_confusion_matrix(y_test, y_pred)
-    best_params = gbtc_cv.best_params_
-
-    # Print out Gradient Boosted Tree report
-    print('-Gradient Boosted Tree Report-')
-    print('\tGradeint Boosted Tree overall accuracy: ', accuracy)
-    print('\tBest parameters:')
-    print('\t\tBest Learning Rate: ', best_params['learning_rate'])
-    print('\t\tBest N Estimators: ', best_params['n_estimators'])
-    print('\t\tBest Max Depth: ', best_params['max_depth'])
-    print('\tMulticlass metrics:')
-
-    # Print out individual class metrics
-    for confusion in confusion_matricies:
-        tp = confusion[0][0]
-        fp = confusion[1][0]
-        fn = confusion[0][1]
-        tn = confusion[1][1]
-
-        accuracy = (tp + tn) / (tp + fp + fn + tn)
-        recall = tp / (tp + fn)
-        specificity = tn / (tn + fp)
-        precision = tp / (tp + fp)
-
-        print('\t\t<label> accuracy: ', accuracy)
-        print('\t\t<label> recall: ', recall)
-        print('\t\t<label> specificity: ', specificity)
-        print('\t\t<label> precision: ', precision)
-
-
-def plot_spectrogram(Y, sr, hop_length, y_axis="linear"):
-    plt.figure(figsize=(25, 10))
-    librosa.display.specshow(Y,
+def plot_mel_spectrogram(y, sr, hop_length):
+    fig = plt.figure(figsize=(25, 10))
+    librosa.display.specshow(y,
                              sr=sr,
                              hop_length=hop_length,
                              x_axis="time",
-                             y_axis=y_axis)
+                             y_axis="log")
     plt.colorbar(format="%+2.f")
-    plt.title("Spectrogram with decibel log", fontsize=24)
+    plt.title("Mel-Spectrogram with decibel log", fontsize=24)
     plt.ylabel("Frequency (Hz)", fontsize=24)
     plt.xlabel("Time (s)", fontsize=24)
-    plt.tight_layout()
+    fig.tight_layout()
+    plt.subplots_adjust(left=0.07, right=1.0)
     plt.show()
 
 
 def main():
-    gtzan_dataset = MusicDataset('../data/gtzan/genres_original')
+    gtzan_dataset = MusicDataset('../data/genres_original')
     gtzan_dataset.load_music()
+    track_no = random.randint(0, 999)
+    track = gtzan_dataset.mfccs[track_no].cpu().detach().numpy()
+    # plot_mel_spectrogram(track, sr=gtzan_dataset.audio_sample_rate, hop_length=256)
+    # plot_mfccs(track, sr=gtzan_dataset.audio_sample_rate, title=f"MFCCs of track no {track_no}, genre {gtzan_dataset.target[track_no]}")
+    print(gtzan_dataset.mfccs.shape)
+
     le = LabelEncoder()
     le.fit(gtzan_dataset.target)
     target = le.transform(gtzan_dataset.target)
-    gradient_boosted(np.array(gtzan_dataset.music_dataset), target)
+    onehot_encoded_target = one_hot(torch.from_numpy(target)).to(device)
+    onehot_encoded_target = onehot_encoded_target.double()
+
+    full_data = list(zip(gtzan_dataset.mfccs, onehot_encoded_target))
+    random.shuffle(full_data)
+
+    training = full_data[:int(len(full_data) * 0.8)]
+    testing = full_data[int(len(full_data) * 0.8):]
+
+    my_lstm = MyLSTM(gtzan_dataset.mfccs.shape[2], gtzan_dataset.mfccs.shape[1], 2, 10).to(device)
+
+    criterion = CrossEntropyLoss()
+    optimizer = torch.optim.Adam(my_lstm.parameters(), lr=0.001)
+    num_epochs = 25
+    for epoch in range(num_epochs):
+        for index, data in enumerate(training):
+            output = my_lstm(torch.unsqueeze(data[0], 0))
+            loss = criterion(output, torch.unsqueeze(data[1], 0))
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (index + 1) % 100 == 0:
+                print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+    # Test the model
+    # In test phase, we don't need to compute gradients (for memory efficiency)
+    with torch.no_grad():
+        n_correct = 0
+        n_samples = 0
+        for index, data in enumerate(testing):
+            output = my_lstm(torch.unsqueeze(data[0], 0))
+
+            predicted = torch.argmax(output)
+            true = torch.argmax(data[1])
+            n_samples += 1
+            if true == predicted:
+                n_correct += 1
+
+        acc = 100.0 * n_correct / n_samples
+        print(f'Accuracy: {acc} %')
 
 
 if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     main()
